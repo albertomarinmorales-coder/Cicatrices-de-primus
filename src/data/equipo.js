a1385fe092a147cd4240d45eee65296b77f9bdc1
albmastrive <alberto.marin@strivelabs.io>
2026-05-12 03:59:25 +0200

    SE ME OLVIDO DAVE

--- src/data/equipo.js
+++ src/data/equipo.js
@@ -1,6 +1,7 @@
 export const STAFF_MEMBERS = [
   { id: 'staff-1', name: 'Dalnir', image: '/assets/images/Staff/Dalnir.png' },
   { id: 'staff-2', name: 'Dario', image: '/assets/images/Staff/Dario.png' },
+  { id: 'staff-dave', name: 'Dave', image: '/assets/images/Staff/dave.png' },
   { id: 'staff-3', name: 'Eselia', image: '/assets/images/Staff/Eselia.png' },
   { id: 'staff-4', name: 'Maekar', image: '/assets/images/Staff/Maekar.png' },
   { id: 'staff-5', name: 'Sola', image: '/assets/images/Staff/Sola.png' },
